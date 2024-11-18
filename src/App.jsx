5063576ec1c8f0961a982e0792c7a9942f05ec18
delowarmi <md48735002@gmail.com>
2024-11-18 09:16:54 +0600

    add region

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  let [all,setAll] = useState([])
+  useEffect(()=>{
+    async function all(){
+      let data = await axios.get( "https://restcountries.com/v3.1/all " );
+      setAll(data.data);
+      // console.log(data);
+    }
+      all() 
+    
+  },[])
   
   return (
     <>
      
+     <select>
+       {all.map(item =>
+        <option>{item.region}</option>
+       )}
+     </select>
+
+
     </>
   )
 }
